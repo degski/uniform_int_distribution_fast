0e31ec131893e8874051f1e1b2a00627a8a40f5f
degski <degski@gmail.com>
2018-08-14 10:15:55 +0300

    gcc x86 test results, added test-result-files and removed them from the readme (too large)

--- benchmark/build_gcc.cmd
+++
@@ -1,1 +0,0 @@
-g++ -o benchmark.exe main.cpp -O3 -std=c++17 -Wno-attributes -m64 -march=broadwell -mtune=broadwell -lbenchmark -lbenchmark_main -lshlwapi
---
+++ benchmark/build_gcc_64.cmd
@@ -0,0 +1,1 @@
+g++ -o g64.exe main.cpp -O3 -std=c++17 -Wno-attributes -m64 -march=broadwell -mtune=broadwell -lbenchmark -lbenchmark_main -lshlwapi
